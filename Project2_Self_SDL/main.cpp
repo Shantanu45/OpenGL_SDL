6a93e0aed692feac9647283d56019cce3ac79850
Shantanu45 <rajatmaurya34@gmail.com>
2021-01-19 00:29:43 +0530

    added shader, compile and link functions

--- Project2_Self_SDL/main.cpp
+++ Project2_Self_SDL/main.cpp
@@ -1,22 +1,132 @@
 #include <Windows.h>
 #include <iostream>
 #include <SDL/SDL.h>
+#include <GL/glew.h>
 
 #include "GameState.h"
 #include "SDL/cSDL.h"
 #include "GLEW/cGLEW.h"
-#include "Game/GameLoop.h"
+
+const GLint width = 1024;
+const GLint height = 768;
+
+GLuint VAO, VBO, shader;
+
+// Vertex Shader code
+static const char* vShader = "                                                \n\
+#version 330                                                                  \n\
+                                                                              \n\
+layout (location = 0) in vec3 pos;											  \n\
+                                                                              \n\
+void main()                                                                   \n\
+{                                                                             \n\
+    gl_Position = vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0);				  \n\
+}";
+
+// Fragment Shader
+static const char* fShader = "                                                \n\
+#version 330                                                                  \n\
+                                                                              \n\
+out vec4 colour;                                                               \n\
+                                                                              \n\
+void main()                                                                   \n\
+{                                                                             \n\
+    colour = vec4(1.0, 0.0, 0.0, 1.0);                                         \n\
+}";
+
+void CreateTriangle()
+{
+	GLfloat vertices[] = {
+		-1.0f, -1.0f, 0.0f,
+		 1.0f, -1.0f, 0.0f,
+		 0.0f,  1.0f, 0.0f
+	};
+	
+	glGenVertexArrays(1, &VAO);		// Vertex Array Object
+	glBindVertexArray(VAO);
+
+	glGenBuffers(1, &VBO);			// Vertex Buffer Object
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+	
+}
+
+void AddShader(GLuint theProgram, const char* shaderCode, GLenum shaderType)
+{
+	GLuint theShader = glCreateShader(shaderType);
+
+	const GLchar* theCode[1];
+	theCode[0] = shaderCode;
+
+	GLint codeLength[1];
+	codeLength[0] = strlen(shaderCode);
+
+	glShaderSource(theShader, 1, theCode, codeLength);
+	glCompileShader(theShader);
+
+	GLint result = 0;
+	GLchar eLog[1024] = { 0 };
+
+	glGetShaderiv(theShader, GL_COMPILE_STATUS, &result);
+	if (!result) 
+	{
+		glGetShaderInfoLog(theShader, 1024, NULL, eLog);
+		fprintf(stderr, "Error compiling the %d shader: '%s'\n", shaderType, eLog);
+		return;
+	}
+
+	glAttachShader(theProgram, theShader);
+}
+
+void CompileShaders()
+{
+	shader = glCreateProgram();
+
+	if (!shader) 
+	{
+		printf("Failed to create shader\n");
+		return;
+	}
+
+	AddShader(shader, vShader, GL_VERTEX_SHADER);
+	AddShader(shader, fShader, GL_FRAGMENT_SHADER);
+
+	GLint result = 0;
+	GLchar eLog[1024] = { 0 };
+
+	glLinkProgram(shader);
+	glGetProgramiv(shader, GL_LINK_STATUS, &result);
+	if (!result) 
+	{
+		glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
+		printf("Error linking program: '%s'\n", eLog);
+		return;
+	}
+
+	glValidateProgram(shader);
+	glGetProgramiv(shader, GL_VALIDATE_STATUS, &result);
+	if (!result) 
+	{
+		glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
+		printf("Error validating program: '%s'\n", eLog);
+		return;
+	}
+
+}
 
 int main(int argc, char** argv)
 {
-	const int width = 1024;
-	const int height = 768;
 	SDL_Window* mainWindow = nullptr;
 	
 	auto _game_state = GameState::PLAY;
 	cSDL* sdl = new cSDL();
 	cGLEW* glew = new cGLEW();
-	GameLoop* gameLoop = new GameLoop();
 	
 	// Initialize SDL
 	if(sdl->initialize(mainWindow, width, height) == -1)
@@ -30,8 +140,26 @@ int main(int argc, char** argv)
 		return EXIT_FAILURE;
 	}
 
-	// main game loop
-	gameLoop->start(_game_state, mainWindow, cSDL::poll);
+	CreateTriangle();
+	CompileShaders();
+
+	while(_game_state != GameState::EXIT)
+	{
+		auto evnt = sdl->poll(_game_state);
+
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glUseProgram(shader);
+
+		glBindVertexArray(VAO);				// because of VAO, here we don't have to bind VBO and attribarray
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);
+		
+		glUseProgram(0);
+
+		SDL_GL_SwapWindow(mainWindow);
+	}
 	
 	return EXIT_SUCCESS;
 }
